e47943bf55d07a09529160f2c98b046064c7e143
AquisTech <anand.bait@gmail.com>
2021-04-25 12:41:23 -0400

    Rename aquis_accounts to hospital-info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "aquis_accounts_production"
+  # config.active_job.queue_name_prefix = "hospital_info_production"
 
   config.action_mailer.perform_caching = false
 
