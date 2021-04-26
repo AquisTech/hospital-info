a4c3a10b48f2fba5f0bd299dcae6eccc991c8c70
AquisTech <anand.bait@gmail.com>
2021-04-26 02:26:35 -0400

    Changes for incomplete contact numbers

--- app/models/contact_number.rb
+++ app/models/contact_number.rb
@@ -1,6 +1,28 @@
 class ContactNumber < ApplicationRecord
-  contact_number_attributes :number
+  attr_accessor :number_isd_code, :number_without_isd_code
+
   belongs_to :contactable, polymorphic: true
 
   enum contact_type: { phone: 0, mobile: 1, fax: 2 }
+
+  def number_isd_code
+    @number_isd_code ||= number && number[0..3]
+  end
+
+  def number_isd_code=(val)
+    @number_isd_code = val
+    self.number = "#{val}#{number_without_isd_code}"
+  end
+  def number_without_isd_code
+    @number_without_isd_code ||= number && number[4..-1]
+  end
+  def number_without_isd_code=(val)
+    @number_without_isd_code = val
+    self.number = "#{number_isd_code}#{val}"
+  end
+  validates :number_isd_code, presence: true, if: proc { |model| model.number_without_isd_code.present? }
+  validates :number_without_isd_code, presence: true, if: proc { |model| model.number_isd_code.present? }
+  # validates :number, phony_plausible: true, if: proc { |model|
+  #   model.number_isd_code.present? && model.number_without_isd_code.present?
+  # }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Create admin user
+puts 'Create admin user...'
 admin = User.where(email: 'admin@example.com').first_or_initialize
 admin.password = 'admin@123'
 admin.password_confirmation = 'admin@123'
@@ -17,4 +17,47 @@ if admin.save
   puts 'Admin created!'
 else
   puts "Admin creation failed! #{admin.errors.full_messages.to_sentence}"
+puts 'Populating Hospitals...'
+
+require 'csv'
+
+CSV.foreach('db/data/hospitals-mini.csv', headers: true, col_sep: '~') do |row|
+  data = row.to_h
+  h_data = {
+    name: data["Hospital_Name"],
+    addresses_attributes: {
+      0 => {
+        address_type: "main",
+        primary: true,
+        address_line_1: "#{data['Building']}, #{data['street']}",
+        address_line_2: data['landmark'],
+        area: data['area'],
+        city: data['city'],
+        district: '',
+        state: data['state'],
+        country: "India",
+        pincode: data['pincode']
+      }
+    }
+  }
+
+  landlines = data['landline'].to_s.split(',')
+  landlines.each_with_index do |landline, index|
+    h_data[:contact_numbers_attributes] ||= {}
+    h_data[:contact_numbers_attributes][index] = {contact_type: "phone", note: "Landline", number_isd_code: "0091", number_without_isd_code: landline}
+  end
+  landlines_count = landlines.size
+  mobiles = data['mobile'].to_s.split(',')
+  mobiles.each_with_index do |mobile, index|
+    h_data[:contact_numbers_attributes] ||= {}
+    h_data[:contact_numbers_attributes][landlines_count + index + 1] = {contact_type: "mobile", note: "", number_isd_code: "0091", number_without_isd_code: mobile}
+  end
+  h_data[:email_addresses_attributes] = {0 => {email_type: "main", email: data['email']}} if data['email'].present?
+  h = Hospital.where(name: data["Hospital_Name"]).first_or_initialize
+  h.assign_attributes(h_data)
+  if h.save
+    puts "Saved #{h.name}"
+  else
+    puts "Failed: #{h.errors.full_messages.to_sentence}"
+  end
 end
